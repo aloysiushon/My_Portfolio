16b5164341679066f12faed6a855b0a365eb19ac
yonglin hon <aloysiushon@hotmail.com>
2023-10-07 20:27:33 +0800

    Add typed.js

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
+// Connects to data-controller="typed-js"
+export default class extends Controller {
+  connect() {
+    console.log(this.element);
+    new Typed(this.element, {
+      strings: ["Fullstack Developer"],
+      typeSpeed: 50,
+      backSpeed: 25,
+      loop: true,
+    })
+  }
+}
